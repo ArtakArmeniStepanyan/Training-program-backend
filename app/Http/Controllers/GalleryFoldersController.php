591b6f28fe73d47cd02688c5f36f3f8e05e55425
Artak Stepanyan <artak.stepanyan.test@gmail.com>
2022-12-21 21:44:11 +0400

    gallery first version

--- app/Http/Controllers/GalleryFoldersController.php
+++ app/Http/Controllers/GalleryFoldersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\GalleryFolders;
+use App\Models\GalleryImages;
 use Illuminate\Http\Request;
 
 class GalleryFoldersController extends Controller
@@ -20,6 +21,117 @@ class GalleryFoldersController extends Controller
                 'status' => 'ok',
                 'message' => 'Folder created',
                 'folders' => $folders
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Something went wrong'
+            ]);
+        }
+    }
+
+    public  function getUserFolders($id){
+        $userFolders = GalleryFolders::where('userId', $id)->get();
+
+        if ($userFolders) {
+            return response()->json([
+                'status' => 'ok',
+                'folders' => $userFolders
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Something went wrong'
+            ]);
+        }
+    }
+
+    public  function getCurrentFolder($id){
+        $folder = GalleryFolders::where('id', $id)->first();
+
+        if ($folder) {
+            return response()->json([
+                'status' => 'ok',
+                'folder' => $folder
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Something went wrong'
+            ]);
+        }
+    }
+
+    public function getCurrentImages($id)
+    {
+        $images = GalleryImages::where('folderId', $id)->get();
+
+        if ($images) {
+            return response()->json([
+                'status' => 'ok',
+                'images' => $images
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Something went wrong'
+            ]);
+        }
+    }
+
+    public  function deleteFolder($id){
+        $folder = GalleryFolders::where('id', $id)->delete();
+        GalleryImages::where('folderId', $id)->delete();
+
+        if ($folder) {
+            return response()->json([
+                'status' => 'ok',
+                'message' => 'Folder deleted'
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Something went wrong'
+            ]);
+        }
+    }
+
+    public function deleteImage($id)
+    {
+        $folderId = GalleryImages::where('id', $id)->first()->folderId;
+        $deletedImage = GalleryImages::where('id', $id)->delete();
+        $images = GalleryImages::where('folderId', $folderId)->get();
+
+        if ($deletedImage) {
+            return response()->json([
+                'status' => 'ok',
+                'message' => 'Image deleted',
+                'images' => $images
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Something went wrong'
+            ]);
+        }
+    }
+
+    public function saveImage(Request $request)
+    {
+        $path = $request->file('image')->store('Gallery_images', 'public');
+
+        $image = GalleryImages::insert([
+            'folderId' => $request->folderId,
+            'image' => $path,
+        ]);
+
+        $images = GalleryImages::where('folderId', $request->folderId)->get();
+
+        if ($image) {
+            return response()->json([
+                'status' => 'ok',
+                'message' => 'Image added',
+                'images' => $images,
             ]);
         } else {
             return response()->json([
--- routes/web.php
+++ routes/web.php
@@ -24,29 +24,23 @@ Route::get('/', function () {
 });
 Route::post('/login', LogController::class . '@login');
 Route::delete('/login/{id}', LogController::class . '@logout');
-
 Route::get('/login/{token}', LogController::class . '@getUser');
 
 Route::get('/check-mail-origin/{email}', UserController::class . '@checkOriginEmail');
 Route::post('/check-mail-origin', UserController::class . '@checkOriginEmailWithRegisteredUser');
-
-
-
 Route::post('/registration', UserController::class . '@registration');
 Route::post('/edit-profile', UserController::class . '@editProfile');
-
 Route::post('/edit-pass', UserController::class . '@editPass');
+Route::delete('/remove-image/{id}', UserController::class . '@removeImage');
+Route::get('/users/{id}', UserController::class . '@getAllUsers');
+Route::get('/users', UserController::class . '@getAllUsersForGuest');
+Route::get('/user/{id}', UserController::class . '@getUser');
 
 Route::post('/add-to-friend', FriendsController::class . '@addToFriend');
 Route::post('/remove-from-friend', FriendsController::class . '@removeFromFriend');
 Route::post('/is-friend', FriendsController::class . '@isFriend');
-
-Route::delete('/remove-image/{id}', UserController::class . '@removeImage');
-
-Route::get('/users/{id}', UserController::class . '@getAllUsers');
-Route::get('/users', UserController::class . '@getAllUsersForGuest');
-Route::get('/user/{id}', UserController::class . '@getUser');
 Route::get('/friends/{id}', FriendsController::class . '@getFriends');
+
 
 Route::post('/add-todo', TodosController::class . '@addTodo');
 Route::get('/get-todos/{id}', TodosController::class . '@getTodos');
@@ -55,3 +49,10 @@ Route::post('/change-todo-status', TodosController::class . '@changeTodoStatus')
 Route::post('/delete-todo', TodosController::class . '@deleteTodo');
 
 Route::post('/create-folder', GalleryFoldersController::class . '@createFolder');
+Route::get('/get-folders/{id}', GalleryFoldersController::class . '@getUserFolders');
+Route::get('/get-current-folder/{id}', GalleryFoldersController::class . '@getCurrentFolder');
+Route::get('/get-current-images/{id}', GalleryFoldersController::class . '@getCurrentImages');
+Route::get('/delete-folder/{id}', GalleryFoldersController::class . '@deleteFolder');
+Route::get('/delete-image/{id}', GalleryFoldersController::class . '@deleteImage');
+Route::post('/save-image', GalleryFoldersController::class . '@saveImage');
+
